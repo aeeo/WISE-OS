a2f9191d46aca6e707eb418994818eccb23734ce
稚澳赵 <51157457+geneedyou@users.noreply.github.com>
2022-01-15 09:26:28 +0800

    updata

---
+++ WISE-Fork/jeecg-boot/jeecg-boot-module-bbs/src/main/java/org/jeecg/modules/GirlFriend.java
@@ -0,0 +1,58 @@
+package org.jeecg.modules;
+
+public class GirlFriend {
+    private String name;
+    private String height;
+    private String weight;
+    private String measurements;
+    private String boyfriend;
+    private String contactInformation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(String measurements) {
+        this.measurements = measurements;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getBoyfriend() {
+        return boyfriend;
+    }
+
+    public void setBoyfriend(String boyfriend) {
+        this.boyfriend = boyfriend;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+}
---
+++ WISE-Fork/jeecg-boot/jeecg-boot-module-bbs/src/main/java/org/jeecg/modules/OfficialWebsite.java
@@ -0,0 +1,15 @@
+package org.jeecg.modules;
+
+//官网代码片段
+public class OfficialWebsite {
+    public static void main(String[] args) {
+        GirlFriend girlFriend = new GirlFriend();
+        girlFriend.setName("刘亦菲");
+        girlFriend.setHeight("170 cm");
+        girlFriend.setWeight("48kg");
+        girlFriend.setMeasurements("83,60,84");
+        girlFriend.setBoyfriend("小赵");
+        girlFriend.setContactInformation("行星万象微信小程序");
+        System.out.println("刘亦菲说快来这个小程序来找我吧。");
+    }
+}
